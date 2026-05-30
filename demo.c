5e45b2bf22348ac23ec80863443cae9b8a911433
lakshya sharma <sharma.lakshya7042@gmail.com>
2026-05-30 12:51:47 +0530

    Add files via upload
    progammes practice

---
+++ demo.c
@@ -0,0 +1,157 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n, square;
+//     printf("enter n:");
+//     scanf("%d",&n);
+//     square = n*n;
+//     printf("square=%d",square);
+//     return 0;
+// }
+// {
+//     int n, cube;
+//     printf("enter n:");
+//     scanf("%d", &n);
+//     cube =n*n*n;
+//     printf("cube=%d",cube);
+//     return 0;
+// }
+// {
+//     int l,b,area;
+//     printf("enter l:");
+//     scanf("%d", &l);
+//     printf("enter b:");
+//     scanf("%d", &b);
+//     area= (l*b)*0.5;
+//     printf("area=%d", area);
+//     return 0;
+// }
+
+// {
+//     int r;
+//     float area;
+//     printf("enter r:");
+//     scanf("%d", &r);
+//     area = 3.14*r*r;
+//     printf("area=%f", area);
+//     return 0;
+// }
+// {
+//     int r;
+//     int h;
+//     float volume;
+//     printf("enter r:");
+//     scanf("%d", &r);
+//     printf("enter h:");
+//     scanf("%d", &h);
+//     volume = (3.14*r*r*h);
+//     printf("volume =%f", volume);
+//     return 0;
+// }
+// {
+//     int sec;
+//     float min, hr;
+//     printf("enter sec:");
+//     scanf("%d", &sec);
+//     min =sec/60;
+//     hr = sec/3600;
+//     printf("min=%f\n", min);
+//     printf("hr=%f", hr);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a,b,c;
+//     float r;
+//     printf("enter a:");
+//     scanf("%d", &a);
+//     printf("enter b:");
+//     scanf("%d", &b);
+//     printf("enter c:");
+//     scanf("%d", &c);
+//     r=(a+b+c)/3.0*100;
+//     if(r>33)
+//     {
+//         printf("pass");
+//     }
+//     else
+//     {
+//         printf("fail");
+//     }
+//     return 0;
+// }
+
+// {
+//     int temp;
+//     printf("enter temp:");
+//     scanf("%d", &temp);
+//     if (temp>=35)
+//     {
+//         printf("hot");
+//     }
+//     else
+//     {
+//         printf("not hot");
+//     }
+//     return 0;
+// }
+
+// {
+//     int item;
+//     printf("enter item price:");
+//     scanf("%d", &item);
+//     if (item>=5000)
+//     {
+//         printf("expensive");
+//     }
+//     else
+//     {
+//         printf("cheap");
+//     }
+//     return 0;
+// }
+
+// {
+//     int p, r, t;
+//     float ci, si;
+//     printf("enter principal:");
+//     scanf("%d", &p);
+//     printf("enter rate:");
+//     scanf("%d", &r);
+//     printf("enter time:");
+//     scanf("%d", &t);
+//     si = (p*r*t)/100.0;
+//     ci = p*((1+r/100.0)*t) - p;
+//     printf("si=%f", si);
+//     printf(" ci=%f", ci);
+//     return 0;
+// }
+// #include<stdio.h>
+// #include<math.h>
+// int main()
+// {
+//     int s,a,b,c;
+//     float area;
+//     printf("enter a:");
+//     scanf("%d", &a);
+//     printf("enter b:");
+//     scanf("%d", &b);
+//     printf("enter c:");
+//     scanf("%d", &c);
+//     s=(a+b+c)/2;
+//     area = sqrt(s*(s-a)*(s-b)*(s-c));
+//     printf("area=%f", area);
+//     return 0;
+// }
+#include<stdio.h>
+int main()
+// {
+//     float gallons;
+//     printf("enter gallons:");
+//     scanf("%f", &gallons); //convert gallons to cubic foot
+//     gallons = gallons * 0.134;
+//     printf("converted gallons to cubic foot=%f", gallons);
+//     return 0;
+// }
