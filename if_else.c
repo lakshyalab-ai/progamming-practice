a66adb5142b9ca99bf12950d0f8285adb788a181
lakshya sharma <sharma.lakshya7042@gmail.com>
2026-06-01 12:57:08 +0530

    Add files via upload
    statement base code

---
+++ if_else.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+// {
+//     int call;
+//     float bill ,tax ,total, rate;
+//     printf("enter call:");
+//     scanf("%d", &call);
+//     if(call<200)
+//     {
+//         rate=1.80;
+//     }
+//     else if(call>=200 && call<500)
+//     {
+//         rate=1.60;
+//     }
+//     else
+//     {
+//         rate= 1.50;
+//     }
+//     bill=call*rate;
+//     tax=bill*7/100;
+//     total=bill+tax;
+//     printf("  bill=%f", bill);
+//     printf("   tax=%f", tax);
+//     printf("     total=%f", total);
+//     return 0;
+// }
+
+
+// {
+//     int salary;
+//     int hra, da,total;
+//     printf("enter salary:");
+//     scanf("%d", &salary);
+//     if(salary<5000 && salary>0)
+//     {
+//         hra=salary*5/100;
+//         da=salary*7/100;
+//         total=salary+hra+da;
+//     }
+//     else if(salary>=5000 && salary<10000)
+//     {
+//         hra=salary*6/100;
+//         da=salary*8/100;
+//         total=salary+hra+da;
+//     }
+//     else{
+//         printf("invalid salary");
+//     }
+//     printf("  hra=%d", hra);
+//     printf("   da=%d", da);
+//     printf("     total salary=%d", total);
+//     return 0;
+// }
